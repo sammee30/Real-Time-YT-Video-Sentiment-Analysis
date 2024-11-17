4401512e0adc40ecac747836f83946d2be409ef8
Vikas <vikas.sahu75@gmail.com>
2024-11-17 12:20:31 +0530

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Its a crome plugin for real time video comments sentiment and trend analysis.',
+    author='Vikas',
+    license='',
+)
